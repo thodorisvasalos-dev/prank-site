2c9c5d1a32c9c859711d115e410b0492bb5e5a90
thodorisvasalos-dev <thodorisvasalos@gmail.com>
2026-07-14 09:39:24 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -7,24 +7,29 @@ const mediaElement = document.getElementById('media');
 const backButton = document.getElementById('backButton');
 
 if(name === "Στέλιος"){
-    greetElement.innerText = "Στέλιο, έχω μια απορία:";
-
-  
-        mediaElement.innerHTML = '<img src = "./assets/whyareyougay.jpg">'
+    greetElement.innerText = "Έχω μια απορία:";
+    mediaElement.innerHTML = '<img src = "./assets/whyareyougay.jpg">'
 
 }else if(name === "Μαρία"){
     greetElement.innerText = "Δυστυχώς, η αλήθεια δεν κρύβεται...";
-
-
-        mediaElement.innerHTML = '<img src = "./assets/adopted.jpg">'
+    mediaElement.innerHTML = '<img src = "./assets/adopted.jpg">'
 
 }
 else if (name === "Μαίρη"){
     greetElement.innerText = "Βρήκα μια σπάνια φωτογραφία σου.";
+    mediaElement.innerHTML = '<img src = "./assets/clown.jpg">'
 
-  
-        mediaElement.innerHTML = '<img src = "./assets/clown.jpg">'
-
+}
+else if (name === 'Ευθύμης'){
+    greetElement.innerText = "Αν ήσουν καρτούν θα ήσουν:";
+    mediaElement.innerHTML = '<img src = "./assets/squidward.jpg">'
+}
+else if(name === 'Θανάσης'){
+    greetElement.innerText = "Πετώντας μαλακίες δεν γίνεσαι πιλότος!!";
+}
+else if(name === 'Νίκωνας'){
+    greetElement.innerText = "Πούλα την λαδιέρα. Πάρε κάτι τέτοιο καλύτερα:";
+    mediaElement.innerHTML = '<img src = "./assets/i30n.jpg" style = "transform: scale(0.6);">'
 }
 else{
     mediaElement.innerHTML = '<img src = "./assets/waitaminute.jpg">'
